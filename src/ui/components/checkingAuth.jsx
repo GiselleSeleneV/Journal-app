3a03ffc0840cac852b7ffc66fe4a4f00a8b718e6
Selene Vargas <dev.front3@talentum.edu.co>
2026-08-13 16:02:23 -0500

    se arregla el error al recargar la app cuando esta desplegada en vercel y no estoy logueda

--- src/ui/components/checkingAuth.jsx
+++ src/ui/components/checkingAuth.jsx
@@ -14,7 +14,7 @@ export const CheckingAuth = () => {
                 direction='row'
                 justifyContent={'center'}
             >
-                <CircularProgress color="warning" />
+                <CircularProgress sx={{ color: '#C9A96E' }} />
             </Grid>
         </Grid>
     )
